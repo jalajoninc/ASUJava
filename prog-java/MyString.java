8929cadc8e3eb724a1d06aa5a884c7f1c065b732
jalajoninc <jalajoninc@gmail.com>
2026-03-23 08:47:00 -0700

    ok

--- prog-java/MyString.java
+++ prog-java/MyString.java
@@ -2,16 +2,38 @@
 
 public class MyString {
     public static void main(String[] args) {
- 
-        // String s1 = "j1";
-        // Character c1 = s1.charAt(0);
-        // Character c2 = s1.charAt(1);
 
-        // if (( c2 > '0' ) && ( c2 < '9' ) ){
-        //     System.out.println( "true 26" );
-        // }
-        test2();
+        sortIt();
    }
+
+public static void sortIt() {
+    int one = 7;
+    int two = 5;
+    int three = 2;
+
+    if ( one > two ) {
+      int tmp  = one;
+      one = two;
+      two = tmp;
+    } 
+ 
+    if ( two > three ) {
+      int tmp  = two;
+      two = three;
+      three = tmp;
+    } 
+
+    if ( one > two ) {
+      int tmp  = one;
+      one = two;
+      two = tmp;
+    } 
+
+
+    System.out.printf( "%c %d %x", 'a', (int) 'a', (int) 'a' );
+    System.out.printf( "%c %d", 'a', (int) 'a' );
+
+}
 
 
 public static void test2() {
